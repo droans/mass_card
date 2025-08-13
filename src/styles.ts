b005365220ebdf7cbb2b31e9004752e3ecb4f034
Michael Carroll <mjoecarroll@gmail.com>
2025-08-13 14:12:40 -0400

    New CSS Vars

--- src/styles.ts
+++ src/styles.ts
@@ -12,8 +12,14 @@ export default css`
   }
   .mass-panel {
     --expansion-panel-content-padding: 0px;
+    --md-list-container-color: rgba(0,0,0,0) !important;
+    --md-list-item-leading-space: 0px;
     --md-list-item-two-line-container-height: 48px;
   }
+    --md-ripple-hover-color: var(--mdc-theme-primary);
+    --mdc-ripple-hover-color: var(--mdc-theme-primary);
+    --mdc-ripple-color: var(--mdc-theme-primary);
+    --md-ripple-color: var(--mdc-theme-primary);
   .main {
     display: flex;
     height: 100%;
